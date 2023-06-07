c775abeea2012abbaef6ab40506f02b55438fd16
wangzhaoming <wangzhaoming1991@gmail.com>
2023-06-07 10:42:08 +0800

    #3 #4

--- bundle.js
+++ bundle.js
@@ -1,23 +1,23 @@
 // load font
 document.fonts.load(`12px pixel`, "测试").catch((e) => {
-    console.error(e);
+  console.error(e);
 });
 
 // create slider
 const
-    range = document.getElementById('thd'),
-    rangeV = document.getElementById('rangeV'),
-    setValue = (e) => {
-        const
-            newValue = Number((range.value - range.min) * 100 / (range.max - range.min)),
-            newPosition = 10 - (newValue * 0.2);
-        rangeV.innerHTML = `<span>${range.value}</span>`;
-        rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
+  range = document.getElementById('thd'),
+  rangeV = document.getElementById('rangeV'),
+  setValue = (e) => {
+    const
+      newValue = Number((range.value - range.min) * 100 / (range.max - range.min)),
+      newPosition = 10 - (newValue * 0.2);
+    rangeV.innerHTML = `<span>${range.value}</span>`;
+    rangeV.style.left = `calc(${newValue}% + (${newPosition}px))`;
 
-        if (e.type === 'input') {
-            draw();
-        }
-    };
+    if (e.type === 'input') {
+      draw();
+    }
+  };
 document.addEventListener("DOMContentLoaded", setValue);
 range.addEventListener('input', setValue);
 
@@ -25,79 +25,108 @@ range.addEventListener('input', setValue);
 const { createPopup } = window.picmoPopup;
 
 document.addEventListener('DOMContentLoaded', () => {
-  const emoji = document.querySelector('#bg');
-  const trigger = document.querySelector('.emoji-button');
+  const bgEmojiTxt = document.querySelector('#bg');
+  const bgEmojiTrigger = document.querySelector('.emoji-button-bg');
 
-  const picker = createPopup({}, {
-    referenceElement: trigger,
-    triggerElement: trigger,
+  const bgEmojiPicker = createPopup({}, {
+    referenceElement: bgEmojiTrigger,
+    triggerElement: bgEmojiTrigger,
     position: 'right-end'
   });
 
-  trigger.addEventListener('click', () => {
-    picker.toggle();
+  bgEmojiTrigger.addEventListener('click', () => {
+    bgEmojiPicker.toggle();
   });
 
-  picker.addEventListener('emoji:select', (selection) => {
-    emoji.value = selection.emoji;
+  bgEmojiPicker.addEventListener('emoji:select', (selection) => {
+    bgEmojiTxt.value = selection.emoji;
   });
 
-  const emoji1 = document.querySelector('#fg');
-  const trigger1 = document.querySelector('.emoji-button1');
+  const fgEmojiTxt = document.querySelector('#fg');
+  const fgEmojiTrigger = document.querySelector('.emoji-button-fg');
 
-  const picker1 = createPopup({}, {
-    referenceElement: trigger1,
-    triggerElement: trigger1,
+  const fgEmojiPicker = createPopup({}, {
+    referenceElement: fgEmojiTrigger,
+    triggerElement: fgEmojiTrigger,
     position: 'right-end'
   });
 
-  trigger1.addEventListener('click', () => {
-    picker1.toggle();
+  fgEmojiTrigger.addEventListener('click', () => {
+    fgEmojiPicker.toggle();
   });
 
-  picker1.addEventListener('emoji:select', (selection) => {
-    emoji1.value = selection.emoji;
+  fgEmojiPicker.addEventListener('emoji:select', (selection) => {
+    fgEmojiTxt.value = selection.emoji;
   });
 });
 
+// copy
+function copy() {
+  const text = document.getElementById('ta').innerText;
+
+  if (!text) {
+    Toastify({
+      text: "请先绘制",
+      duration: 2000
+    }).showToast();
+    return;
+  }
+
+  navigator.clipboard.writeText(text);
+  Toastify({
+    text: "Emoji Copied!",
+    duration: 2000
+  }).showToast();
+}
 
 // canvas
 const canvas = document.getElementById('content');
-canvas.width = 240;
-canvas.height = 12;
 
 function draw() {
-    //获得 2d 上下文对象
-    const ctx = canvas.getContext('2d');
+  //获得 2d 上下文对象
+  const ctx = canvas.getContext('2d');
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    const text = document.getElementById('in').value;
-    const bgtext = document.getElementById('bg').value;
-    const fgtext = document.getElementById('fg').value;
-    const thd = document.getElementById('thd').value;
-    const l = text.length * 12;
-
-    ctx.fillStyle = "rgb(0, 0, 0)";
-    ctx.fillRect(0, 0, l, 12);
+  const text = document.getElementById('in').value;
+  const bgtext = document.getElementById('bg').value;
+  const fgtext = document.getElementById('fg').value;
+  const thd = document.getElementById('thd').value;
+  const colPixels = text.length * 12;
+  const rowPixels = 1 * 12;
 
 
-    ctx.font = "12px pixel"
-    ctx.fillStyle = "rgb(255, 255, 255)";
-    ctx.textBaseline = 'top';
+  canvas.width = colPixels;
+  canvas.height = rowPixels;
 
-    ctx.fillText(text, 0, 0);
+  const containerWidth = canvas.parentElement.offsetWidth;
+  canvas.style.height = canvas.height * 3 + 'px';
+  canvas.style.width = canvas.width * 3 + 'px';
+  if (canvas.width * 3 > containerWidth) {
+    canvas.style.width = containerWidth + 'px';
+    canvas.style.height = canvas.height / canvas.width * containerWidth + 'px';
+  }
 
-    const myImageData = ctx.getImageData(0, 0, l, 12);
-    console.log(myImageData);
-    let v = '';
-    for (let i = 0; i < 12; i++) {
-        for (let j = 0; j < l; j++) {
-            const n = i * l * 4 + j * 4;
-            v += myImageData.data[n] > thd ? fgtext : bgtext;
-        }
-        v += '\n';
+  ctx.fillStyle = "rgb(0, 0, 0)";
+  ctx.fillRect(0, 0, colPixels, 12);
+
+
+  ctx.font = "12px pixel"
+  ctx.fillStyle = "rgb(255, 255, 255)";
+  ctx.textBaseline = 'top';
+
+  ctx.fillText(text, 0, 0);
+
+  const myImageData = ctx.getImageData(0, 0, colPixels, 12);
+  console.log(myImageData);
+  let v = '';
+  for (let i = 0; i < 12; i++) {
+    for (let j = 0; j < colPixels; j++) {
+      const n = i * colPixels * 4 + j * 4;
+      v += myImageData.data[n] > thd ? fgtext : bgtext;
     }
-    console.log(v);
-    document.getElementById("ta").innerText = v;
+    v += '\n';
+  }
+  console.log(v);
+  document.getElementById("ta").innerText = v;
 }
